f8345b523bef3b9cb459b76256ead3c4be45f7d3
unknown <igorcvetkovic006@gmail.com>
2023-01-08 11:45:40 +0100

    ispravka pri pravljenju ToBackStack-a

--- app/src/main/java/com/example/arclass/HomeFragment.java
+++ app/src/main/java/com/example/arclass/HomeFragment.java
@@ -2,11 +2,16 @@ package com.example.arclass;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
@@ -15,6 +20,8 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 }
